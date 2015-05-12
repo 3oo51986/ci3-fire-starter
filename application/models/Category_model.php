567b2ab50b711f10f75c8dc1aaaaba2edb2d0458
Nguyễn Tuấn Siêu <oliversieu@gmail.com>
2015-05-13 03:25:02 +0700

    Customize

---
+++ application/models/Category_model.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Category_model extends MY_Model {
+    public $_table = 'category';
+    public $primary_key = 'id';
+}
